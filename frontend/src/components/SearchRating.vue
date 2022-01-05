cdd85ee574428a16fa4ac63bda62659e071852e8
Goncalves-Rafael <rafael-goncalves-oliveira@outlook.com>
2022-01-05 18:01:38 +0000

    Adiciona componente para registro de feedback

---
+++ frontend/src/components/SearchRating.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-flex flex-column align-center">
+    <p>Quão satisfeito você ficou com o resultado dessa busca?</p>
+    <v-rating
+      v-if="!ratingSubmitted"
+      background-color="grey"
+      color="primary"
+      empty-icon="mdi-star-outline"
+      full-icon="mdi-star"
+      hover
+      length="5"
+      size="24"
+      :value="rating"
+      @input="setRating"
+    ></v-rating>
+    <p v-else>Obrigado pela avaliação.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchRating",
+  computed: {
+    rating: function () {
+      return this.$store.getters.rating;
+    },
+    ratingSubmitted: function () {
+      return this.rating !== null;
+    },
+  },
+  methods: {
+    setRating(value) {
+      this.$store.dispatch("setRating", value);
+    },
+  },
+};
+</script>
