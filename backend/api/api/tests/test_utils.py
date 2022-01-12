e8f347518bc715359a6950d8e13fdcbbbc41ccec
Luiz Felipe Nery <luizfelipemdnery@gmail.com>
2022-01-12 15:13:39 -0300

    Resolvendo bug e limpando comentrios inuteis

--- backend/api/api/tests/test_utils.py
+++ backend/api/api/tests/test_utils.py
@@ -39,28 +39,3 @@ class ViewTestCase(TestCase):
         self.assertEqual(
             FilterUtils.generate_filters(params),
             filters)
-
-
-    # def set_response(self, url, param):
-    #     """Create response from parameter"""
-    #     client = APIClient()
-    #     return client.get(
-    #         url,
-    #         param,
-    #         format='json')
-
-    # def test_api_wrong_url(self):
-    #     """Test the api has handle wrong url capability."""
-    #     wrong_url = '/any'
-    #     wrong_param = {'ingredient': 'tomate'}
-    #     self.assertEqual(
-    #         self.set_response(wrong_url, wrong_param).status_code,
-    #         status.HTTP_404_NOT_FOUND)
-
-    # def test_api_recipes_list_wrong_param(self):
-    #     """Test the api has handle wrong parameters capability."""
-    #     url = '/recipes/'
-    #     param = {'wrong': 'even more wrong'}
-    #     self.assertEqual(
-    #         self.set_response(url, param).status_code,
-    #         status.HTTP_400_BAD_REQUEST)
