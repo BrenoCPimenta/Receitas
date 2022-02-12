50010b2afbdb9ba97e6fb645981017898ec22aba
Goncalves-Rafael <rafael-goncalves-oliveira@outlook.com>
2022-02-12 16:56:24 -0300

    Adiciona container para o backend

---
+++ backend/wait-elastic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:9200  )
+echo "Waiting for elasticsearch..."
+while [ $response != "200" ]
+do
+    sleep 5;
+    response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:9200  )
+done
+
+echo "Elastic search started"
+
+cd database/indexer
+sudo python3 update_mapping.py -force_reindexation recipes
+
+cd ../../api
+python3 manage.py runserver
