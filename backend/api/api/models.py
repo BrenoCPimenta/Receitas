4fc1c8fc85eae57b999f22c1ea7d1fdfaed0a3d1
Luiz Felipe Nery <luizfelipemdnery@gmail.com>
2022-02-16 15:58:56 -0300

    Criando modelo de usuario

--- backend/api/api/models.py
+++ backend/api/api/models.py
@@ -1,2 +1,11 @@
 """Modules.
 """
+from django.db import models
+
+class User(models.Model):
+    """User model.
+    """
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    name = models.CharField(max_length=50)
